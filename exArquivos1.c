7fcd58e000123baafe69ce0eb2e158838ae46670
Gabriel Phillipy <gbrielphil@gmail.com>
2025-07-10 10:47:12 -0300

    Add files via upload

--- exArquivos1.c
+++ exArquivos1.c
@@ -24,14 +24,14 @@ void preencherNotas(float notas[][3], char nomeArquivo[]){
         return;
     }
     //
-    int cont = 0;
     for(int i = 0; i < 10; i++){
-        for(int j = 0; j < 3; j++ ){
-            while(fscanf(arq, "%f", &notas[i][j]) == 3){
-                cont++;
+        for(int j = 1; j < 3; j++ ){
+            if(fscanf(arq, "%f", &notas[i][j]) != 1){
+                fclose(arq);
+                return;
             }
         }
-    }
+    } fclose(arq);
 }
 //
 void gerarRelatorio(float notas[][3]){
